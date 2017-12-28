587a96fe02ab35b315c95b63e15d501b7fffe01a
Yunus Yurttagul <yunuss@gmail.com>
2017-12-28 03:54:52 +0300

    Some nice changes with appereance

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { ScrollView, Dimensions, StyleSheet, Text, View, TouchableHighlight, ListView, RefreshControl } from 'react-native';
+import { Alert, FlatList, Dimensions, StyleSheet, Text, View, TouchableHighlight, ListView, RefreshControl } from 'react-native';
 import api from './utilities';
 
 let winSize = Dimensions.get('window');
 
-class Elements extends Component {
-  handlePress(){
+class MyListItem extends Component {
+  // show alert on press ( symbol + price $)
+  handlePress(i,j){
+    Alert.alert(i + ' $' + j ); 
     //console.log('press');
   }
   handleLongPress(){
@@ -13,22 +15,28 @@ class Elements extends Component {
   }
   
   render(){
+    let textColor = this.props.change[0]== "-" ? "red" : "green";
+    
     return(   
-      this.props.value.map((element, index) => (
         <TouchableHighlight
           style={styles.TouchablePinBodyChild}
-          key={index} 
-          onPress={this.handlePress}
+          key={this.props.id} 
+          onPress={this.handlePress.bind(this, this.props.symbol, this.props.price)}
           onLongPress={this.handleLongPress}>
           <View style={styles.PinBodyChild}>
-            <View style={styles.PinBodyChildText}>
-              <Text style={styles.text}>{index+1}){element.symbol}</Text>
+            <View style={styles.PinBody1Child}>
+              <Text style={styles.text}>{this.props.id+1})</Text>
             </View>
-            <View style={styles.PinBodyChildText}><Text style={styles.text}>{element.price_usd}</Text></View>
-            <View style={styles.PinBodyChildText}><Text style={styles.text}>{element.percent_change_24h}</Text></View>
+            <View style={styles.PinBody2Child}>
+              <View style={styles.PinBody2GrandChild}>
+                <Text style={styles.text}>{this.props.symbol}</Text>
+                <Text style={styles.subText}>{this.props.name}</Text>
+              </View>
+            </View>
+            <View style={styles.PinBody3Child}><Text style={styles.text}>${this.props.price}</Text></View>
+            <View style={styles.PinBody4Child}><Text style={styles.text}><Text style={{color: textColor}}>%{this.props.change}</Text></Text></View>
           </View>
         </TouchableHighlight>
-      ))
     );
   }
 }
@@ -63,6 +71,28 @@ export default class App extends Component {
     });
   }
 
+  _keyExtractor = (item, index) => item.id;
+
+  _renderItem = ({item, index}) => (
+    <MyListItem
+      id={index}
+      symbol={item.symbol}
+      name={item.name}
+      price={item.price_usd}
+      change= {item.percent_change_24h}
+    />
+  );
+
+  _renderSeparator(){ 
+    return(
+      <View
+        style={{
+          backgroundColor: 'wheat',
+          height: 1,
+        }}
+      />
+    );
+  }
   render() {
     if (this.state.isLoading) {
       return (<View style={styles.LoadingContainer}><Text>Loading...</Text></View>)
@@ -70,12 +100,17 @@ export default class App extends Component {
       return(
         <View style={styles.PinContainer}>
           <View style={styles.PinHeader}>
-            <View style={styles.PinBodyChildText}><Text style={styles.PinHeaderText}>Type</Text></View>
-            <View style={styles.PinBodyChildText}><Text style={styles.PinHeaderText}>Price</Text></View>
-            <View style={styles.PinBodyChildText}><Text style={styles.PinHeaderText}>% Change</Text></View>
+            <View style={styles.PinHeader1Child}><Text style={styles.PinHeaderText}>#</Text></View>
+            <View style={styles.PinHeader2Child}><Text style={styles.PinHeaderText}>Coin</Text></View>
+            <View style={styles.PinHeader3Child}><Text style={styles.PinHeaderText}>Price</Text></View>
+            <View style={styles.PinHeader4Child}><Text style={styles.PinHeaderText}>Change(24H)</Text></View>
           </View>
           <View style={styles.PinBody}>
-            <ScrollView 
+            <FlatList 
+              ItemSeparatorComponent={this._renderSeparator}
+              data={this.state.cryptoData}
+              keyExtractor={this._keyExtractor}
+              renderItem={this._renderItem}
               refreshControl={
                 <RefreshControl
                   refreshing={this.state.refreshing}
@@ -84,13 +119,7 @@ export default class App extends Component {
                   title={'Release to Refresh...'}
                 />
               }
-            >
-              <Elements value={this.state.cryptoData} />
-            </ScrollView>
-          </View>
-             
-          <View style={styles.PinFooter}>
-
+            />
           </View>
         </View> 
       )}
@@ -99,7 +128,6 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   PinContainer: {
     flex: 1,
-    backgroundColor: '#cecece',
     alignSelf: 'stretch',
   },
   LoadingContainer: {
@@ -113,27 +141,33 @@ const styles = StyleSheet.create({
     marginRight:10,
     marginTop: 30,
     flexDirection: 'row',
-    alignItems: 'flex-end',
-    justifyContent: 'space-between',
+    alignItems: 'flex-start',
     padding: 7,
-    borderRadius: 15,
-    borderWidth: 2,
-    borderColor: 'steelblue',
-    backgroundColor: 'skyblue' 
+    borderBottomWidth: 2,
+    borderBottomColor: '#555555' 
+  },
+  PinHeader1Child: {
+    flex:1.5,
+  },
+  PinHeader2Child: {
+    flex:3.5,  
+  },
+  PinHeader3Child: {
+    flex:6,
+  },
+  PinHeader4Child: {
+    flex:5,
   },
   PinHeaderText: {
     color: '#555555',
-    fontSize: 40/winSize.scale,
-    fontWeight: 'bold'
+    fontSize: 35/winSize.scale,
+    fontFamily: 'GillSans-Bold'
   },
   PinBody: {
     flex: 7,
     marginLeft:10,
     marginRight:10,
-    borderRadius:15,
-    borderColor: 'wheat',
-    borderWidth: 2,
-    backgroundColor: 'whitesmoke',
+    backgroundColor: 'white',
     flexDirection: 'column',
     justifyContent: 'space-between'
   },
@@ -141,22 +175,39 @@ const styles = StyleSheet.create({
     borderRadius: 15
   },
   PinBodyChild: {
+    height: 60,
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    padding: 8
+    alignItems: 'flex-start',
+    padding: 8,
+    marginTop: 20
+  },
+  PinBody1Child: {
+    flex:1.7,
+  },
+  PinBody2Child: {
+    flex:3.5,
+  },
+  PinBody2GrandChild: {
+    flexDirection: 'column'
+  },
+  PinBody3Child: {
+    flex:6,
+  },
+  PinBody4Child: {
+    flex:5,
   },
   PinBodyChildText: {
     flexDirection: 'row',
     alignItems: 'flex-start',
     flex: 1,
   },
-  PinFooter: {
-    flex: 2,
-    backgroundColor: 'white',
-  },
-
   text: {
     color: '#555555',
-    fontSize: 40/winSize.scale,
+    fontSize: 42/winSize.scale,
+    fontFamily: 'Gill Sans'
+  },
+  subText: {
+    fontSize: 20/winSize.scale,
+    fontFamily: 'GillSans-Light'
   }
 });
